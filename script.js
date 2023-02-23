1ac3bd1a3d994547c7bf046c7a4baabf0ae011f8
mary ann <114210058+maryannintech@users.noreply.github.com>
2023-02-23 13:16:38 +0800

    update javascript

--- script.js
+++ script.js
@@ -10,28 +10,27 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection.toLowerCase();
     let result = ""
     if (playerSelection == 'rock' && computerSelection == 'paper') {
-        result = `You lost! Paper beat Rock \nPlayer: ${userscore} \nComputer: ${++computerscore}`;   
+        result = `You lost! Paper beat Rock. \nPlayer: ${userscore} \nComputer: ${++computerscore}`;   
     }
     else if (playerSelection == 'rock' && computerSelection == 'scissor') {
-        result = `You won! Rock beat Scissor \nPlayer: ${++userscore} \nComputer: ${computerscore}`;     
+        result = `You won! Rock beat Scissor. \nPlayer: ${++userscore} \nComputer: ${computerscore}`;     
     }
     else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        result = `You won! Paper beat Rock \nPlayer: ${++userscore} \nComputer: ${computerscore}`;     
+        result = `You won! Paper beat Rock. \nPlayer: ${++userscore} \nComputer: ${computerscore}`;     
     }
     else if (playerSelection == 'paper' && computerSelection == 'scissor') {
-        result = `You lost! Scissor beat Paper \nPlayer: ${userscore} \nComputer: ${++computerscore}`;     
+        result = `You lost! Scissor beat Paper. \nPlayer: ${userscore} \nComputer: ${++computerscore}`;     
     }
     else if (playerSelection == 'scissor' && computerSelection == 'rock') {
-        result = `You lost! Rock beat Scissor \nPlayer: ${userscore} \nComputer: ${++computerscore}`;     
+        result = `You lost! Rock beat Scissor. \nPlayer: ${userscore} \nComputer: ${++computerscore}`;     
     }
     else if (playerSelection == 'scissor' && computerSelection == 'paper') {
-        result = `You won! Scissor beat Paper \nPlayer: ${++userscore} \nComputer: ${computerscore}`;     
+        result = `You won! Scissor beat Paper. \nPlayer: ${++userscore} \nComputer: ${computerscore}`;     
     }
     else {
-        result = `Tie\n Player: ${userscore} \nComputer: ${computerscore}`;
+        result = `Tie \nPlayer: ${userscore} \nComputer: ${computerscore}`;
     }
     return result;
 }
@@ -56,10 +55,9 @@ function checkWinner() {
 
 function game() {
     for (let round = 1; round < 6; round++) {
-        let user = prompt("Rock, Paper, or Scissor?");
+        let user = prompt("Rock, Paper, or Scissor?").toLowerCase();
         let computer = getComputerChoice();
-        console.log(`----Round (${round})---- \nYou chose ${user} \nThe computer ${computer}`);
-        playRound(user,computer);
+        console.log(`----Round (${round})---- \nYou chose ${user} \nThe computer ${computer} \n\n----Results---- \n${playRound(user,computer)}`);
     }
 
     finalwinner = checkWinner();
