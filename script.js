a47d93ab17391f7b5a3ade2f8a9dc3c32b1bc6a0
mary ann <114210058+maryannintech@users.noreply.github.com>
2023-02-23 12:29:08 +0800

    update script.js

--- script.js
+++ script.js
@@ -1,43 +1,65 @@
 // generate a random choice
 function getComputerChoice() {
-    let selection = ['Rock', 'Paper', 'Scissor'];
+    let selection = ['rock', 'paper', 'scissor'];
     let computerchoice = selection[Math.floor(Math.random()*selection.length)];
     return computerchoice;
 }
 
+// d
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase(playerSelection);
+    playerSelection.toLowerCase();
     let result = ""
-    if (playerSelection == 'rock' && computerSelection == 'Paper') {
-        result = "You lost! Paper beat Rock";
+    if (playerSelection == 'rock' && computerSelection == 'paper') {
+        result = `You lost! Paper beat Rock \nPlayer: ${userscore} \nComputer: ${++computerscore}`;   
     }
-    else if (playerSelection == 'rock' && computerSelection == 'Scissor') {
-        result = "You won! Rock beat Scissor";
+    else if (playerSelection == 'rock' && computerSelection == 'scissor') {
+        result = `You won! Rock beat Scissor \nPlayer: ${++userscore} \nComputer: ${computerscore}`;     
     }
-    else if (playerSelection == 'paper' && computerSelection == 'Rock') {
-        result = "You won! Paper beat Rock";
+    else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        result = `You won! Paper beat Rock \nPlayer: ${++userscore} \nComputer: ${computerscore}`;     
     }
-    else if (playerSelection == 'paper' && computerSelection == 'Scissor') {
-        result = "You lost! Scissor bear Paper";
+    else if (playerSelection == 'paper' && computerSelection == 'scissor') {
+        result = `You lost! Scissor beat Paper \nPlayer: ${userscore} \nComputer: ${++computerscore}`;     
     }
-    else if (playerSelection == 'scissor' && computerSelection == 'Rock') {
-        result = "You lost! Rock beat Scissor";
+    else if (playerSelection == 'scissor' && computerSelection == 'rock') {
+        result = `You lost! Rock beat Scissor \nPlayer: ${userscore} \nComputer: ${++computerscore}`;     
     }
-    else if (playerSelection == 'scissor' && computerSelection == 'Paper') {
-        result = "You won! Scissor beat Paper";
+    else if (playerSelection == 'scissor' && computerSelection == 'paper') {
+        result = `You won! Scissor beat Paper \nPlayer: ${++userscore} \nComputer: ${computerscore}`;     
     }
     else {
-        result = "Tied";
+        result = `Tie\n Player: ${userscore} \nComputer: ${computerscore}`;
     }
     return result;
 }
 
 function game() {
+    let user = prompt("Rock, Paper, or Scissor?");
+    let computer = getComputerChoice();
+    let win = `You won! You beat the Computer. ${scoreboard}`;
+    let lose = `You lost! Better luck next time. ${scoreboard}`;
+    let tie = `It's a tie!`
+    let winner = "";
+
+    for (let i = 0; i < 5; i++) {
+        playRound(user,computer);
+    }
     
+    if (userscore > computerscore) {
+        winner = win;
+    }
+    else if (userscore < computerscore) {
+        winner = lose;
+    }
+    else {
+        winner = tie;
+    }
+
+    return winner;
 }
 
+let userscore = 0;
+let computerscore = 0;
+let scoreboard = `\nPlayer: ${userscore} \nComputer: ${computerscore}`;
 
-let user = prompt("Rock, Paper, or Scissor?");
-computer = getComputerChoice();
-
-console.log(playRound(user, computer));
+console.log(game());
