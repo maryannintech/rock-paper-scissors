9aef92de454e3818664d67cb41b03d28dce81755
mary ann <114210058+maryannintech@users.noreply.github.com>
2023-02-23 11:23:27 +0800

    update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let user = prompt("Rock, Paper, or Scissor?");
-
 // generate a random choice
 function getComputerChoice() {
     let selection = ['Rock', 'Paper', 'Scissor'];
@@ -8,11 +6,38 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == 'Rock' && computerSelection == 'Paper') {
-        
+    playerSelection = playerSelection.toLowerCase(playerSelection);
+    let result = ""
+    if (playerSelection == 'rock' && computerSelection == 'Paper') {
+        result = "You lost! Paper beat Rock";
     }
+    else if (playerSelection == 'rock' && computerSelection == 'Scissor') {
+        result = "You won! Rock beat Scissor";
+    }
+    else if (playerSelection == 'paper' && computerSelection == 'Rock') {
+        result = "You won! Paper beat Rock";
+    }
+    else if (playerSelection == 'paper' && computerSelection == 'Scissor') {
+        result = "You lost! Scissor bear Paper";
+    }
+    else if (playerSelection == 'scissor' && computerSelection == 'Rock') {
+        result = "You lost! Rock beat Scissor";
+    }
+    else if (playerSelection == 'scissor' && computerSelection == 'Paper') {
+        result = "You won! Scissor beat Paper";
+    }
+    else {
+        result = "Tied";
+    }
+    return result;
+}
+
+function game() {
+    
 }
 
 
+let user = prompt("Rock, Paper, or Scissor?");
+computer = getComputerChoice();
 
-console.log(getComputerChoice());
+console.log(playRound(user, computer));
